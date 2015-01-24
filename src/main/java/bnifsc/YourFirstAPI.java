73ae1b1c42f69eb5fe925b426bea9cc531fe9a2e
vinaymavi <vinaymavi@gmail.com>
2015-01-25 04:01:28 +0530

    Google Endpoint working

--- src/main/java/bnifsc/YourFirstAPI.java
+++ src/main/java/bnifsc/YourFirstAPI.java
@@ -1,8 +1,28 @@
 package bnifsc;
 
-/**
-  * Add your first API methods in this class, or you may create another class. In that case, please
-  * update your web.xml accordingly.
- **/
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+/** An endpoint class we are exposing */
+@Api(name = "bnifsc",
+     version = "v1",
+     scopes = {Constants.EMAIL_SCOPE},
+     clientIds = {Constants.WEB_CLIENT_ID},    		    
+     namespace = @ApiNamespace(ownerDomain = "helloworld.example.com",
+                                ownerName = "helloworld.example.com",
+                                packagePath=""))
 public class YourFirstAPI {
+
+    /** A simple endpoint method that takes a name and says Hi back */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name);
+
+        return response;
+    }
+
 }

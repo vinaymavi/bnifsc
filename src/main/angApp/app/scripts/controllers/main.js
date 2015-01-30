25438ae453a44b8ea7d999c6c8b811b1c940a52a
vinaymavi <vinaymavi@gmail.com>
2015-01-30 21:13:30 +0530

    Bootstrap css version added

---
+++ src/main/angApp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bnifscApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bnifscApp
+ */
+angular.module('bnifscApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

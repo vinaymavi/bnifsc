d52cd66aa01ff7d9ccc15bc2c9abe628d35b17f2
vinaymavi <vinaymavi@gmail.com>
2015-02-07 15:28:54 +0530

    Blank bulkuploader added

---
+++ src/main/java/bnifsc/util/BulkUploadMapper.java
@@ -0,0 +1,8 @@
+package bnifsc.util;
+
+import com.google.appengine.tools.mapreduce.Mapper;
+import com.google.appengine.tools.mapreduce.inputs.GoogleCloudStorageLineInput;
+
+public class BulkUploadMapper  {
+
+}

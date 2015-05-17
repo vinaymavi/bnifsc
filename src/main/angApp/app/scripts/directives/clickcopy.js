2d4976d9a98ae94d562cc382d7fbb5fa55aae92d
vinay mavi <vinaymavi@gmail.com>
2015-05-17 18:42:26 +0530

    copy to clip board api pending in between

--- src/main/angApp/app/scripts/directives/clickcopy.js
+++ src/main/angApp/app/scripts/directives/clickcopy.js
@@ -8,12 +8,18 @@
  * register click event and copy text to clipboard.
  */
 angular.module('bnifscApp')
-  .directive('clickCopy', function () {
+  .directive('clickCopy', function ($window) {
     return {
-      template: '<div></div>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        element.text('this is the clickCopy directive');
+          element.click(function($event){
+              console.log("click event");
+              console.log(scope);
+              console.log(element.text());
+              console.log($window);
+              console.log($event);
+          })
+        console.log(element);
       }
     };
   });

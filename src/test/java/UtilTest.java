8c85aa3be14a0c6d708140746d0b375d822145c2
vinay mavi <vinaymavi@gmail.com>
2015-12-19 17:07:25 +0530

    Test case configured.

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -1,5 +1,14 @@
 /**
  * Created by vinaymavi on 19/12/15.
  */
-public class UtilTest {
+import junit.framework.TestCase;
+import org.apache.commons.lang3.text.WordUtils;
+
+public class UtilTest extends TestCase {
+    public void testCapitalize(){
+        String name = "ABHYUDAYA COOPERATIVE BANK LIMITED";
+        String nameSmall = name.toLowerCase();
+        String nameCapitalize = WordUtils.capitalize(nameSmall);
+        assertEquals("Abhyudaya Cooperative Bank Limited",nameCapitalize);
+    }
 }

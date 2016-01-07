2f25930ef6104aabcd97453fac824cf07d14d2cb
vinay mavi <vinaymavi@gmail.com>
2016-01-08 00:38:08 +0530

    UI router:
    Angular -ui router configured.
    
    fixed#66

--- src/main/angApp/app/scripts/app.js
+++ src/main/angApp/app/scripts/app.js
@@ -8,40 +8,34 @@
  * Main module of the application.
  */
 angular
-  .module('bnifscApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ngMaterial'
-  ])
-  .config(function($routeProvider,$locationProvider) {
-    $routeProvider
-      .when('/:bank?/:state?/:district?/:branch?/:ifsc?', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl as ctrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    .module('bnifscApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ngMaterial',
+        'ui.router'
+    ])
+    .config(function ($routeProvider, $locationProvider, $stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+        $stateProvider.state("admin", {
+            url: "/admin",
+            templateUrl: "views/admin.html",
+            controller: "AdminCtrl"
+        })
 
-      // Set hashbang mode mode.
-      $locationProvider.hashPrefix('!');
-  });
-
+        // Set hashbang mode mode.
+        $locationProvider.hashPrefix('!');
+    });
 
 
 function init() {
-  var ROOT = 'https://bnifsc-beta.appspot.com/_ah/api';
-  gapi.client.load('bnifsc', 'v2', function() {
-    window.init();
-  }, ROOT);
+    var ROOT = 'https://bnifsc-beta.appspot.com/_ah/api';
+    gapi.client.load('bnifsc', 'v2', function () {
+        window.init();
+    }, ROOT);
 }
--- src/main/angApp/app/scripts/controllers/main.js
+++ src/main/angApp/app/scripts/controllers/main.js
@@ -51,7 +51,7 @@ angular.module('bnifscApp')
                     deferred.resolve(resp.branches);
                 });
                 return deferred.promise;
-            }else{
+            } else {
                 return results;
             }
 

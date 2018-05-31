2f5f8c61a00b04831a372451ee2e5b799afce470
VinayMavi <vinaymavi@gmail.com>
2018-05-31 21:23:06 +0530

    Bank model ready.

--- app/app/webapp/models.py
+++ app/app/webapp/models.py
@@ -2,12 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from djangae import fields
 
 class Bank(models.Model):
-    name = models.CharField(max_length=200, default='')
-    image_url = models.CharField(max_length=200, default='', blank=True)
-    acronym = models.CharField(max_length=50, default='', blank=True)
+    name = fields.CharField(max_length=200, default='')
+    image_url = fields.CharField(max_length=200, default='', blank=True)
+    acronym = fields.CharField(max_length=50, default='', blank=True)
     seo_content_top = models.TextField(max_length=1000, blank=True)
     seo_content_middle = models.TextField(max_length=100, blank=True)
     seo_content_bottom = models.TextField(max_length=100, blank=True)

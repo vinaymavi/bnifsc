f6bd069035d2ee4bf35dbf6cf04b2db9b570e94b
VinayMavi <vinaymavi@gmail.com>
2018-06-21 14:11:51 +0530

    bootstrap cdn added.

--- app/app/settings.py
+++ app/app/settings.py
@@ -121,8 +121,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CSP_DEFAULT_SRC = ("'self'", "*.gstatic.com")
 # Inline styles are unsafe, but Django error pages use them. We later remove
 # `unsafe-inline` in settings_live.py
-CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", "fonts.googleapis.com", "*.gstatic.com")
-CSP_FONT_SRC = ("'self'", "themes.googleusercontent.com", "*.gstatic.com")
+CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", "fonts.googleapis.com", "*.gstatic.com","*.bootstrapcdn.com")
+CSP_FONT_SRC = ("'self'", "themes.googleusercontent.com", "*.gstatic.com","*.bootstrapcdn.com")
 CSP_FRAME_SRC = ("'self'", "www.google.com", "www.youtube.com", "accounts.google.com", "apis.google.com", "plus.google.com")
 CSP_SCRIPT_SRC = ("'self'", "*.ampproject.org","*.googleanalytics.com", "*.google-analytics.com", "ajax.googleapis.com")
 CSP_IMG_SRC = ("'self'", "data:", "s.ytimg.com", "*.googleusercontent.com", "*.gstatic.com", "www.google-analytics.com")

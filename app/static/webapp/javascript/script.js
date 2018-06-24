75b760ca3865d5cd5628ea20e5ef1bbbb10628ac
VinayMavi <vinaymavi@gmail.com>
2018-06-24 13:00:48 +0530

    static file updated.

--- app/static/webapp/javascript/script.js
+++ app/static/webapp/javascript/script.js
@@ -1,66 +1,20 @@
 "use strict";
-(function() {
-  const config = {    
-    selector: {
-      bank: "#bank",
-      state: "#state",
-      district: "#district",
-      city: "#city",
-      branch: "#branch"
-    }
-  };
-
+(function() {  
   function register_listener() {
-    const bank_sel = document
-    .querySelector(config.selector.bank);
-    // Bank change listener
-   bank_sel.addEventListener("change", function(event) {
+    document.querySelectorAll('.form-section form select').forEach((select)=>{
+      select.addEventListener("change", function(event) {
         event.preventDefault();
         show_loader();
         event.target.options[event.target.selectedIndex].dataset.url && window.location.assign(event.target.options[event.target.selectedIndex].dataset.url)
       });
-    // State change listener
-    document
-      .querySelector(config.selector.state)
-      .addEventListener("change", function(event) {
-        event.preventDefault();
-        show_loader();        
-        event.target.options[event.target.selectedIndex].dataset.url && window.location.assign(event.target.options[event.target.selectedIndex].dataset.url)
-      });
-
-    // District change listener
-    document
-      .querySelector(config.selector.district)
-      .addEventListener("change", function(event) {
-        event.preventDefault();
-        show_loader();
-        event.target.options[event.target.selectedIndex].dataset.url && window.location.assign(event.target.options[event.target.selectedIndex].dataset.url)
-      });
-
-    // City change listener
-    document
-      .querySelector(config.selector.city)
-      .addEventListener("change", function(event) {
-        event.preventDefault();
-        show_loader();
-        event.target.options[event.target.selectedIndex].dataset.url && window.location.assign(event.target.options[event.target.selectedIndex].dataset.url)
-      });
-
-    // Branch change listener
-    document
-      .querySelector(config.selector.branch)
-      .addEventListener("change", function(event) {
-        event.preventDefault();
-        show_loader();
-        event.target.options[event.target.selectedIndex].dataset.url && window.location.assign(event.target.options[event.target.selectedIndex].dataset.url)
-      });
+    })    
   }
   function show_loader(){
     document.querySelector('.loader').style.display='block';
   }
   function init() {
     register_listener();
-  }  
+  }
 
   document.addEventListener("DOMContentLoaded", () => init());
 })();

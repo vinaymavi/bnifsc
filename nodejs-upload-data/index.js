124b62e0276da5c1354bfb0606af713bd2ea5322
VinayMavi <vinaymavi@gmail.com>
2018-07-02 22:45:57 +0530

    data loaded from big query.

--- nodejs-upload-data/index.js
+++ nodejs-upload-data/index.js
@@ -2,21 +2,26 @@
 // TODO test cases pending.
 // [START app]
 const express = require("express");
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 const PubSub = require("@google-cloud/pubsub");
-const logging  = require('./lib/logging')
+const BigQuery = require("@google-cloud/bigquery");
+
+const logging = require("./lib/logging");
 const app = express();
+const bigquery = new BigQuery();
+
+const query = "SELECT *  FROM `bnifsc-beta:BNIFSC_IFSC_CODE.ifsc_sample_data`";
 
 // Add the request logger before anything else so that it can
 // accurately log requests.
 // [START requests]
-app.use(logging.requestLogger)
+app.use(logging.requestLogger);
 
 app.use(bodyParser.json());
 
 const TOPIC_NAME = "nodejs";
 const pubsub = PubSub({
-  projectId: 'bnifsc-beta'
+  projectId: "bnifsc-beta"
 });
 
 app.get("/", (req, res) => {
@@ -26,7 +31,7 @@ app.get("/", (req, res) => {
     .end();
 });
 
-app.get("/nodejs", (req, res) => {  
+app.get("/nodejs", (req, res) => {
   const topic = pubsub.topic(TOPIC_NAME);
   const publisher = topic.publisher();
   publisher.publish(
@@ -56,16 +61,34 @@ app.get("/nodejs/subscriber", (req, res) => {
     .end();
 });
 
+app.get("/nodejs/bigquery", (req, res) => {
+  bigquery
+    .createQueryStream(query)
+    .on("error", err => {
+      logging.error(JSON.stringify(err));
+      res.status(500).send(JSON.stringify(err));
+    })
+    .on("data", function(row) {
+      logging.info(JSON.stringify(row));
+    })
+    .on("end", function() {
+      logging.info("QUERY STREAM END");
+      res.status(200).send("Data loaded from bigquery.");
+    });
+});
+
 app.post("/nodejs/subscriber", (req, res) => {
   logging.info(JSON.stringify(req.body));
   // data is base64 encoded
-  const message_data = Buffer.from(req.body.message.data,'base64').toString();
+  const message_data = Buffer.from(req.body.message.data, "base64").toString();
   // const message_data = req.body.message.data;
   logging.info("New Build.");
   logging.info(`sent message =${message_data}`);
   res
     .status(200)
-    .send(`Post - subscriber calling, data received=${JSON.stringify(req.body)}`)
+    .send(
+      `Post - subscriber calling, data received=${JSON.stringify(req.body)}`
+    )
     .end();
 });
 

0f5275309ce6b7abc76a1c533e1781e88bf265a5
VinayMavi <vinaymavi@gmail.com>
2018-07-29 17:53:28 +0530

    Bank names publish to pubsub ready.

--- extract/main.py
+++ extract/main.py
@@ -4,6 +4,11 @@ from google.cloud import pubsub_v1
 import base64
 import json
 import logging
+from google.cloud import bigquery
+
+# Instantiates a client
+bigquery_client = bigquery.Client()
+
 logging.getLogger().setLevel(logging.INFO)
 
 PROJECT = 'bnifsc-beta'
@@ -29,23 +34,42 @@ def transform_bank():
     This HTTP request trigger automatically by pub-sub subscriber.
     """
     data = request.get_json()
-    logging.info(data)
+    logging.info("TRANSFORM BANK REQUEST START")
+    logging.info("JSON DATA = {}".format(data))
     if 'message' in data and 'data' in data['message']:
         message_str = data['message']['data']
-        logging.info(message_str)
-        logging.info(base64.b64decode(message_str))
-    logging.info("Transform Bank Calling...")
+        logging.info("ENCODED STRING = {}".format(message_str))
+        logging.info("DECODED STRING = {}".format(base64.b64decode(message_str)))
+    logging.info("TRANSFORM BANK REQUEST END")
     return "Transform Bank Calling..."
 
 @app.route('/extract/transform-bank-push', methods=['GET'])
 def transform_bank_push():
-    logging.info("Transform Bank Push Calling...")
-    for n in range(1, 10):
-        data = u'Message number {}'.format(n)
-        # Data must be a bytestring
-        data = data.encode('utf-8')
-        publisher.publish(topic_path, data=data)
+    """
+    Push Banks data to pub-sub.
+    """
+    logging.info("########## BANK DATA PUBLISHING START#########")
+    query = (
+    'SELECT * FROM `bnifsc-beta.BNIFSC_IFSC_CODE.Dataprep_Bank_List` '
+    'LIMIT 100')
+    query_job = bigquery_client.query(
+        query,
+        # Location must match that of the dataset(s) referenced in the query.
+        location='US')  # API request - starts the query
 
+    for row in query_job:  # API request - fetches results
+        logging.info("******** NEW ROW START**********")
+        logging.info("PUB_SUB:PUSH:BANK_NAME {}".format(row))
+        data = {
+            "bank_id":row['bank_id'],
+            "bank":row['bank'],
+            "old_bank_name":row['old_bank_name'],
+        }
+        data_str = data=json.dumps(data)
+        logging.info("Data = {}".format(data_str))
+        publisher.publish(topic_path,data_str)
+        logging.info("******** NEW ROW END**********")
+    logging.info("########## BANK DATA PUBLISHING STOP#########")        
     return "Transform Bank Push Calling..."
 
 
